a2028f954dd5dafe20d4f97bc23065eccc990308
Doom-Art <ouginak121@gmail.com>
2022-10-20 09:29:20 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+
+namespace FileIO_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string line in File.ReadLines(@"results.txt", Encoding.UTF8))
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
